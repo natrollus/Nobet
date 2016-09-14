351433c88650363c95059f5c386fff41825e24f6
byram <natrollus@gmail.com>
2016-09-15 01:01:26 +0300

    toast olayini static olarak gönderdik bakalim

--- app/src/main/java/com/natrollus/nobet/Widget.java
+++ app/src/main/java/com/natrollus/nobet/Widget.java
@@ -9,6 +9,8 @@ import android.appwidget.*;
 import android.content.*;
 import android.widget.RemoteViews.*;
 
+import static com.natrollus.nobet.araclar.Logla.tostla;
+
 public class Widget extends AppWidgetProvider {
 	RemoteViews rv;
 	AppWidgetManager awm;
@@ -17,15 +19,19 @@ public class Widget extends AppWidgetProvider {
 	
     @Override
     public void onReceive(Context context, Intent intent) {
-		
-		awm = AppWidgetManager.getInstance(context);
-		cn = new ComponentName(context,getClass());
-		rv = new RemoteViews(context.getPackageName(),R.layout.anasayfa);
-		Toast.makeText(context,"s:"+intent.getAction(),Toast.LENGTH_LONG).show();
+
+        ayarla(context);
+        tostla(context,"s:"+intent.getAction());
 		Intent hazirlik = new Intent("selam",null,context,Widget.class);
 		PendingIntent bekleyen = PendingIntent.getBroadcast(context,0,hazirlik,0);
 		rv.setOnClickPendingIntent(R.id.resim,bekleyen);
 		awm.updateAppWidget(cn,rv);
 
+    }
+
+    private void ayarla(Context context) {
+        awm = AppWidgetManager.getInstance(context);
+        cn = new ComponentName(context,getClass());
+        rv = new RemoteViews(context.getPackageName(), R.layout.anasayfa);
     }
 }
--- app/src/main/java/com/natrollus/nobet/araclar/Logla.java
+++ app/src/main/java/com/natrollus/nobet/araclar/Logla.java
@@ -1,9 +1,14 @@
 package com.natrollus.nobet.araclar;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 public class Logla {
     public static void logla(Object o){
         Log.v("bilgi",""+o);
+    }
+    public static void tostla(Context context, Object o){
+        Toast.makeText(context,""+o,Toast.LENGTH_LONG).show();
     }
 }
