2d0b7e1f153c49d82bc21c8acf84c9a594d74569
natrollus <natrollus@gmail.com>
2016-09-15 00:52:53 +0300

    buton ayarlandi ve toast a bildiri yapildi,sonrasinda onRecieve() üzerinden intent aksiyon "selam" olarak alindi

--- app/src/main/java/com/natrollus/nobet/Widget.java
+++ app/src/main/java/com/natrollus/nobet/Widget.java
@@ -3,14 +3,29 @@ package com.natrollus.nobet;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.*;
+import android.app.*;
+import android.appwidget.*;
+import android.content.*;
+import android.widget.RemoteViews.*;
 
 public class Widget extends AppWidgetProvider {
-
+	RemoteViews rv;
+	AppWidgetManager awm;
+	ComponentName cn;
+	
+	
     @Override
     public void onReceive(Context context, Intent intent) {
-
-
-
+		
+		awm = AppWidgetManager.getInstance(context);
+		cn = new ComponentName(context,getClass());
+		rv = new RemoteViews(context.getPackageName(),R.layout.anasayfa);
+		Toast.makeText(context,"s:"+intent.getAction(),Toast.LENGTH_LONG).show();
+		Intent hazirlik = new Intent("selam",null,context,Widget.class);
+		PendingIntent bekleyen = PendingIntent.getBroadcast(context,0,hazirlik,0);
+		rv.setOnClickPendingIntent(R.id.resim,bekleyen);
+		awm.updateAppWidget(cn,rv);
 
     }
 }
