ae0028663e2e78b975e6b351aeb5423b88fc8114
byram <natrollus@gmail.com>
2016-09-14 19:00:21 +0300

    Initial commit

---
+++ app/src/main/java/com/natrollus/nobet/Widget.java
@@ -0,0 +1,16 @@
+package com.natrollus.nobet;
+
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+public class Widget extends AppWidgetProvider {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/natrollus/nobet/araclar/Logla.java
@@ -0,0 +1,9 @@
+package com.natrollus.nobet.araclar;
+
+import android.util.Log;
+
+public class Logla {
+    public static void logla(Object o){
+        Log.v("bilgi",""+o);
+    }
+}
