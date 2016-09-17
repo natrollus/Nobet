d5191c936ea88c9f266b7167109822d6d81255cc
byram <natrollus@gmail.com>
2016-09-17 12:54:04 +0300

    neler oldu neler bitti.. Paylasim.java isimli dosyamiza baska uygulamalardan gelen paylasma hecetinin bilgileri analiz edilip degerlendiriliyor. pardon analiz edilip degerlendirilmiyor.sadece o paylasilan objenin uri si icerigi,text se yazisi falan,ha bu ne isimize yaricak,bu sayede kisayollar daha net bi sekilde widget e aktarilabilinecek,paylasilan objenin kisayolu yapilacak haydi kal saglicakla..

---
+++ app/src/main/java/com/natrollus/nobet/aktivite/Paylasim.java
@@ -0,0 +1,39 @@
+package com.natrollus.nobet.aktivite;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.natrollus.nobet.R;
+
+import static com.natrollus.nobet.araclar.Logla.logla;
+import static com.natrollus.nobet.araclar.Logla.tostla;
+
+public class Paylasim extends Activity {
+
+    TextView cikti;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.paylasim);
+        cikti = (TextView) findViewById(R.id.cikti);
+        Intent icerik = getIntent();
+        String yaz;
+        yaz = "data:" + icerik.getData() + "\ntip:" +icerik.getType() + "\ndatastirng:" + icerik.getDataString();
+        yaz += "\n\n--extras--\n";
+        Bundle bundle = icerik.getExtras();
+        for (String key: bundle.keySet()){
+            Object deger = bundle.get(key);
+            if (deger != null) {
+                yaz += "\nkey:" + key + "\ndeger:" + deger.toString() + "\nclass:" + deger.getClass().getName();
+            }
+
+        }
+
+        cikti.setText(yaz);
+
+
+    }
+}
